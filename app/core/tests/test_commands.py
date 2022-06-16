ef944f7ee73fd9125610357bc4136d8f64f459ed
ash-newton <ash@newton.co>
2022-06-16 14:51:37 -0400

    Configured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -29,7 +29,6 @@ class CommandTests(SimpleTestCase):
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
-        
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-
